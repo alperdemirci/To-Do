803c003fc8e0fd438629aa09812688a26858196a
alperdemirci <alper.demirci@gmail.com>
2017-05-12 16:00:36 -0400

    Pod file added. Firebase connection has been established

---
+++ To Do/FirebaseDataAdapter.swift
@@ -0,0 +1,26 @@
+//
+//  FirebaseDataAdapter.swift
+//  To Do
+//
+//  Created by alper on 5/11/17.
+//  Copyright © 2017 alper. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+enum FirebaseError: Error {
+    case uidNotFound
+}
+
+class FirebaseDataAdapter {
+//    var firebaseRef: FIRDatabaseReference
+//    var videoRef: FIRStorageReference
+//    
+//    init() {
+//        firebaseRef = FIRDatabase.database().reference()
+//        videoRef = FIRStorage.storage().reference().child("user_videos")
+//    }
+    
+
+}
