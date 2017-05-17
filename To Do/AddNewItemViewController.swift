daf2ee7406122f6f181c081115db26699741bae2
alperdemirci <alper.demirci@gmail.com>
2017-05-17 16:13:00 -0400

    New Futures added to the AddNewITemViewController like tet field, button support, switch for share textField

--- To Do/AddNewItemViewController.swift
+++ To Do/AddNewItemViewController.swift
@@ -9,16 +9,44 @@
 import UIKit
 
 class AddNewItemViewController: UIViewController {
-
+    var database = FirebaseDataAdapter()
+    
+    // MARK: Outlets
+    @IBOutlet weak var sharedContinueSwitch: UISwitch!
+    @IBOutlet weak var errorMessage: UILabel!
     @IBOutlet weak var todoTextField: UITextField!
+    @IBOutlet weak var shareEmailTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-        let amountOfLinesToBeShown:CGFloat = 6
-        let maxHeight:CGFloat = todoTextField.font!.lineHeight * amountOfLinesToBeShown
-        todoTextField.sizeThatFits(CGSize(width:todoTextField.frame.size.width, height:maxHeight))
-
+        self.errorMessage.isHidden = true
+        self.sharedContinueSwitch.isOn = false
+        self.navigationController?.navigationBar.isHidden = false
     }
-
+    
+    @IBAction func newTodoAdded(_ sender: Any) {
+        if self.todoTextField.text != "" {
+            self.errorMessage.text = "please write something first"
+            self.errorMessage.isHidden = false
+            self.navigationController?.popViewController(animated: true)
+            
+        }
+        let currentDateTime = Date()
+        if self.sharedContinueSwitch.isOn==true && self.shareEmailTextField.text=="" {
+            self.sharedContinueSwitch.isOn = false
+            self.sharedContinueSwitchChanged(Any)
+            //TODO: ask user if the are sure not to share this todo with a user
+        }
+        self.database.writeTodoIntoDB(value: self.todoTextField.text!, date: currentDateTime, sharedEmail: self.shareEmailTextField.text ?? "" ) { (check) in
+            if check {
+                // TODO: all good
+            } else {
+                // TODO: need to save the todo to phone
+            }
+        }
+    }
+    
+    @IBAction func sharedContinueSwitchChanged(_ sender: Any) {
+        self.shareEmailTextField.isHidden = self.sharedContinueSwitch.isOn == false ?  true : false
+    }
 }
