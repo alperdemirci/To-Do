2e686dc5d8c7a410f6b09c9c60fdb5ebe22565a0
alperdemirci <alper.demirci@gmail.com>
2017-05-17 16:08:59 -0400

    ListItemVIew controller and storyboard has been updated with the new design; segue to addNewViewController as well as, added the tableView cell to be expandable

--- To Do/ListItemTableViewCell.swift
+++ To Do/ListItemTableViewCell.swift
@@ -10,15 +10,32 @@ import UIKit
 
 class ListItemTableViewCell: UITableViewCell {
 
+    @IBOutlet weak var sharedLabelContsraint: NSLayoutConstraint!
+    @IBOutlet weak var todoLabel: UILabel!
+    @IBOutlet weak var sharedLabel: UILabel!
+    @IBOutlet weak var sharedButton: UIButton!
+    
+    weak var delegate: ButtonDelegate?
+    
+    @IBOutlet weak var sharedEmailChange: UIButton!
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
     }
 
+    
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
         // Configure the view for the selected state
     }
 
+    @IBAction func sharedButtonPressed(_ sender: Any) {
+        self.delegate?.sharedButtonPressed(sender as! UIButton, tableViewCell: self)
+    }
+}
+
+protocol ButtonDelegate: class {
+    func sharedButtonPressed(_ userAccept: UIButton, tableViewCell: ListItemTableViewCell)
+    
 }
