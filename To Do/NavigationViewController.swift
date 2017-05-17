c87d2a940d972baad4b9e10f5170f00984f14408
alperdemirci <alper.demirci@gmail.com>
2017-05-17 16:05:28 -0400

    NavigationViewController added to the project to handle the loggedin conditions as well as delegates

---
+++ To Do/NavigationViewController.swift
@@ -0,0 +1,40 @@
+//
+//  NavigationViewController.swift
+//  To Do
+//
+//  Created by alper on 5/16/17.
+//  Copyright © 2017 alper. All rights reserved.
+//
+
+import UIKit
+import Firebase
+//import ZoomTransitioning
+
+class NavigationViewController: UINavigationController {
+
+    let database = FIRDatabase()
+//    private let zoomNavigationControllerDelegate = ZoomNavigationControllerDelegate()
+    
+//    required init?(coder aDecoder: NSCoder) {
+//        super.init(coder: aDecoder)
+//        
+//        delegate = zoomNavigationControllerDelegate
+//    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        if FIRAuth.auth()?.currentUser?.uid == nil {
+            perform(#selector(handleLogout), with: nil, afterDelay: 0)
+        }
+    }
+    
+    func handleLogout() {
+        do {
+            try FIRAuth.auth()?.signOut()
+        } catch let logoutError {
+            print(logoutError)
+        }
+        let initialViewController = InitialViewController()
+        present(initialViewController, animated: true, completion: nil)
+    }
+}
