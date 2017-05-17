d0ab9b23e589fdc5fb77c0665ef8e736966a2971
alperdemirci <alper.demirci@gmail.com>
2017-05-17 16:10:26 -0400

    User nsobject created for the tableView extension as well as TodoExtensions, for time formatting for the Firebase easy store

---
+++ To Do/TodoExtensions.swift
@@ -0,0 +1,50 @@
+//
+//  NSDateISO8601.swift
+//
+//  Created by Justin Makaila on 8/11/14.
+//  Copyright (c) 2014 Present, Inc. All rights reserved.
+//
+
+import Foundation
+import MapKit
+
+public extension Date {
+    public static func ISOStringFromDate(_ date: Date) -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.locale = Locale(identifier: "en_US_POSIX")
+        dateFormatter.timeZone = TimeZone(abbreviation: "GMT")
+        dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSS"
+        
+        return dateFormatter.string(from: date) + "Z"
+    }
+    
+    public static func dateFromISOString(_ string: String) -> Date {
+        let dateFormatter = DateFormatter()
+        dateFormatter.locale = Locale(identifier: "en_US_POSIX")
+        dateFormatter.timeZone = TimeZone.autoupdatingCurrent
+        dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSZ"
+        
+        return dateFormatter.date(from: string)!
+    }
+    
+    public static func offsetFrom(_ date: Date) -> String {
+        
+        let yearmonthdayHourMinuteSecond: NSCalendar.Unit = [.year, .month, .day, .hour, .minute, .second]
+        let difference = (Calendar.current as NSCalendar).components(yearmonthdayHourMinuteSecond, from: date, to: Date(), options: [])
+        
+        let seconds = "\(difference.second!)s"
+        let minutes = "\(difference.minute!)m" + " " + seconds
+        let hours = "\(difference.hour!)h" + " " + minutes
+        let days = "\(difference.day!)d" + " " + hours
+        let months = "\(difference.month!)M" + " " + days
+        let years = "\(difference.year!)Y" + " " + months
+        
+        if difference.year!		> 0 { return years }
+        if difference.month!		> 0 { return months }
+        if difference.day!			> 0 { return days }
+        if difference.hour!		> 0 { return hours }
+        if difference.minute!	> 0 { return minutes }
+        if difference.second!	> 0 { return seconds }
+        return ""
+    }
+}
